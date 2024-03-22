d6bb16358364aa3b9e89ab77699b94dfb4a19139
Samuel José Candido <101359102+SamuelCandido@users.noreply.github.com>
2024-03-22 08:20:49 -0300

    Segunda atividade do Beecrow - Interminado
    Não esta terminado

---
+++ Atividades/Uni3Uri-beecrowd/Uni3Uri2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Uni3Uri2 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        float n = 3.14159f;
+
+        float raio = scanner.nextFloat();
+        
+        double area = n *  Math.pow(raio, raio);
+
+        System.out.println("A= " + ());
+    }
+}
