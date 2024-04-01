bb9bf06a6f2339e811c6f8ac39342847e738a76f
Samuel José Candido <101359102+SamuelCandido@users.noreply.github.com>
2024-04-01 19:28:58 -0300

    Update Uni3Exe12.java

--- Atividades/Uni3Exe-Java/Atividades/Uni3Exe12.java
+++ Atividades/Uni3Exe-Java/Atividades/Uni3Exe12.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
-
 public class Uni3Exe12{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Informe o nome do funcionário: ");
-        String nome = scanner.nextLine();
-        
-        System.out.println("Informe quantas horas trabalhou no mês: ");
-        int horasMes = scanner.nextInt();
-        
-        System.out.println("Informe quantos dependentes possui: ");
+
+        System.out.print("Informe o nome do funcionário: ");
+        String nome = scanner.next();
+
+        System.out.print("Informe quantas horas trabalhou no mês: ");
+        double horasMes = scanner.nextDouble();
+
+        System.out.print("Informe quantos dependentes possui: ");
         int qtdDependentes = scanner.nextInt();
-        
-        int valorHora = 10;
-        int valorDependente = 60;
-        
-        double salarioBruto = horasMes * valorHora + valorDependente * qtdDependentes;
-        double salarioLiquido = salarioBruto - (salarioBruto * 13.5) / 100;
-        
-        System.out.printf("Salário bruto de %s: R$ %.2f%n", nome, salarioBruto);
-        System.out.printf("Salário líquido de %s: R$ %.2f%n", nome, salarioLiquido);
-        
+
+        int salFamil = qtdDependentes * 60;
+        double salBruto = horasMes * 10;
+
+        double inss = salBruto * 0.085;
+        double impostoRenda = salBruto * 0.05;
+        double salLiquido = salBruto - inss - impostoRenda;
+
+        double totalLiquido = salLiquido + salFamil;
+        double totalBruto = salBruto + salFamil;
+
+        System.out.printf("Salário bruto de %s: R$ %.2f%n", nome, totalBruto);
+        System.out.printf("Salário líquido de %s: R$ %.2f%n", nome, totalLiquido);
         scanner.close();
     }
 }
