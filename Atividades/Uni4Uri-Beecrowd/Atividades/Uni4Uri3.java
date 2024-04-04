29428a707506aa911052e76eae135cf72513e42f
Samuel José Candido <101359102+SamuelCandido@users.noreply.github.com>
2024-04-04 23:46:26 +0000

    Comentarios sobre a questao

--- Atividades/Uni4Uri-Beecrowd/Atividades/Uni4Uri3.java
+++ Atividades/Uni4Uri-Beecrowd/Atividades/Uni4Uri3.java
@@ -7,11 +7,20 @@ public class Uni4Uri3 {
         double x = scanner.nextDouble();
         double y = scanner.nextDouble();
 
+        /*
+         * Necessariamente tem que ser nesta ordem para nao 
+         * entrar em outra condição pois se colocar por exemplo x == 0 && y > 0 
+         * irá existir conflito de if a entrada atenderá duas condições 
+         */
 
         if (x == 0 && y == 0){
             System.out.println("Origem");
         }
 
+        // Neste caso se voce eliminar a outra parte ele é obrigado a atender x == 0 
+        // se vc passa essa condição primeiro vc induz a logica e como usasse parenteses
+        // entao para suprir isso ou vc induz a logica ou usa o comando "||"
+        // em resumo ta errada pois quando vc atende a duas condições vc está induzindo o usuario ao erro
         else if (x == 0){
             System.out.println("Eixo Y");
 
