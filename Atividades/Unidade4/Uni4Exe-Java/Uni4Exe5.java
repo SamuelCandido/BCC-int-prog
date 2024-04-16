e5c85cf882e63d034e6fcd40a2daa1bc3ddf91f0
SamuelCandido <samueljosecandido@gmail.com>
2024-04-16 15:35:34 -0300

    Correção, esta incorreta

--- Atividades/Unidade4/Uni4Exe-Java/Uni4Exe5.java
+++ Atividades/Unidade4/Uni4Exe-Java/Uni4Exe5.java
@@ -1,31 +1,20 @@
 import java.util.Scanner;
 
-
-//true e false refaz
 public class Uni4Exe5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite uma cor: ");
-        String cor = scanner.nextLine();
-
-        String corUpper = cor.toUpperCase();
-
-        if (corUpper.equals("AZUL")) {
-            System.out.println();
-            System.out.println("A cor azul é muito bela, olhe para o ceu");
-        } 
+        System.out.println("A cor é azul?");
+        boolean resp = scanner.nextBoolean();
         
-        else if (corUpper.equals("")) {
-            System.out.println();
-            System.out.println("A oque você não digitou nada, veja: " + cor + "  A não esta vendo? Justamente...");
-        }
 
+        if (resp == true){
+            System.out.println("Sim");
+        }
+        
         else{
-            System.out.println();
-            System.out.println("A oque você digitou não foi azul, veja: " + cor);
+            System.out.println("Não");
         }
-
 
         scanner.close();
     }
