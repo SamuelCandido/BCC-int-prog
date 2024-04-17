d2641828ec554752d65f10e15c7b69061938f63e
SamuelCandido <samueljosecandido@gmail.com>
2024-04-17 14:57:36 -0300

    pequenos erros corrigidos

--- Atividades/Unidade5/Uni5Exe-Java/atividades/Uni5Exe1.java
+++ Atividades/Unidade5/Uni5Exe-Java/atividades/Uni5Exe1.java
@@ -1,37 +1,37 @@
 import java.util.Scanner;
 
-public class Uni5Exe1 {public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+public class Uni5Exe1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    for (int i = 0; i < 20; i++) {
-        System.out.println((i + 1) + ")Digite um número1: ");
-        String aux = scanner.nextLine();
+        for (int i = 0; i < 20; i++) {
+            System.out.println((i + 1) + ")Digite um número1: ");
+            String aux = scanner.nextLine();
 
-        try {
-            int num = Integer.parseInt(aux);
-            int resposta = num % 2;
+            try {
+                int num = Integer.parseInt(aux);
+                int resposta = num % 2;
 
-            if (resposta == 0) {
-                System.out.println();
-                System.out.println((i + 1 ) + ")R:O numero "+ num + " é par");
-                System.out.println();
-            }
-            else{
-                System.out.println();
-                System.out.println((i + 1 ) + ")R:O numero "+ num + " é impar");
-                System.out.println();
-            }
+                if (resposta == 0) {
+                    System.out.println();
+                    System.out.println((i + 1 ) + ")R:O numero "+ num + " é par");
+                    System.out.println();
+                }
+                else{
+                    System.out.println();
+                    System.out.println((i + 1 ) + ")R:O numero "+ num + " é impar");
+                    System.out.println();
+                }
+                
+            } 
             
-        } 
-        
-        catch (NumberFormatException e) {
+            catch (NumberFormatException e) {
 
-            System.out.println();
-            System.out.println("ENTRADA INVALIDA");
-            System.out.println();
+                System.out.println();
+                System.out.println("ENTRADA INVALIDA");
+                System.out.println();
+            }
         }
+        scanner.close();
     }
-
-    scanner.close();
-}
 }
--- Atividades/Unidade5/Uni5Exe-Java/atividades/Uni5Exe2.java
+++ Atividades/Unidade5/Uni5Exe-Java/atividades/Uni5Exe2.java
@@ -1,35 +1,31 @@
-import java.util.Scanner;
+public class Uni5Exe2 {
+    public static void main(String[] args) {
 
-public class Uni5Exe2 {public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 100; i++) {
+            int num = i+1;
 
-    for (int i = 0; i < 100; i++) {
-        int num = i+1;
+            try {
+                int resposta = num % 2;
 
-        try {
-            int resposta = num % 2;
-
-            if (resposta == 0) {
-                System.out.println();
-                System.out.println("O numero "+ num + " é par");
-                System.out.println();
-            }
-            else{
-                System.out.println();
-                System.out.println("O numero "+ num + " é impar");
-                System.out.println();
-            }
+                if (resposta == 0) {
+                    System.out.println();
+                    System.out.println("O numero "+ num + " é par");
+                    System.out.println();
+                }
+                else{
+                    System.out.println();
+                    System.out.println("O numero "+ num + " é impar");
+                    System.out.println();
+                }
+                
+            } 
             
-        } 
-        
-        catch (NumberFormatException e) {
+            catch (NumberFormatException e) {
 
-            System.out.println();
-            System.out.println("ENTRADA INVALIDA");
-            System.out.println();
+                System.out.println();
+                System.out.println("ENTRADA INVALIDA");
+                System.out.println();
+            }
         }
     }
-
-    scanner.close();
-}
 }
