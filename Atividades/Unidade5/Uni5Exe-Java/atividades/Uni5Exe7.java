4df9f45e2faca31cb57f5c95c50b6124bcc0da84
Samuel José Candido <samueljosecandido9@gmail.com>
2024-04-19 16:11:13 -0300

    Falta aprender lista

---
+++ Atividades/Unidade5/Uni5Exe-Java/atividades/Uni5Exe7.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Uni5Exe7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quantantos numeros você quer escrever");
+        int qtd = scanner.nextInt();
+
+        double soma = 0.0;
+        double media = 0.0;
+        double numero = 0.0;
+
+        for (int i = 1; i <= qtd; i++) {
+            System.out.println();
+            System.out.println("Escreva a altura da pessoa numero " + i);
+            numero = scanner.nextDouble();
+            //Vai precisar de lista
+
+        }
+
+        System.out.println("A media é " + media);
+
+        scanner.close();
+    }
+}
