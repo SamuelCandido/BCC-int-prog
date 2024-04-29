b3c3edb50bb906a1540797b5d4222db0d72c8285
Samuel José Candido <samueljosecandido9@gmail.com>
2024-04-29 16:39:53 -0300

    Atividade 9 feita

---
+++ Atividades/Unidade5/Uni5Exe-Java/atividades/Uni5Exe9.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Uni5Exe9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quantos alunos você quer digitar");
+        int qtd = scanner.nextInt();
+
+        int idade = 0;
+        int cont = 0;
+
+        ArrayList<String> listaDeNome = new ArrayList<>();
+
+        for (int i = 1; i <= qtd; i++) {
+            System.out.println();
+
+            System.out.println("Escreva o nome do aluno " + i + ": ");
+            String nome = scanner.next();
+
+            System.out.println("Escreva a idade do aluno " + i + ": ");
+            idade = scanner.nextInt();
+
+            if (idade == 18) {
+                listaDeNome.add(nome);
+            }
+
+            else if (idade > 20) {
+                cont += 1;
+            }
+
+            else{
+                System.out.println("Entrada invalida");
+            }
+
+        }
+
+        System.out.println();
+        System.out.println("Lista dos nomes dos alunos que tem 18: ");
+        for(String str : listaDeNome ){
+            System.out.println(str);
+        }
+
+        System.out.println();
+        System.out.println("Quantidade de alunos de idade acima de 20: " + cont);
+        
+
+        scanner.close();
+    }
+}
