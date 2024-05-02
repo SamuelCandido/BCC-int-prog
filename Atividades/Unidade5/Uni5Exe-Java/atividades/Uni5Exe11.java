5e497a0a726637d844c3ad60fefb57581b01b06f
Samuel José Candido <samueljosecandido9@gmail.com>
2024-05-02 14:53:02 -0300

    Feito

---
+++ Atividades/Unidade5/Uni5Exe-Java/atividades/Uni5Exe11.java
@@ -0,0 +1,19 @@
+public class Uni5Exe11 {
+    public static void main(String[] args) {
+        
+        int hora = 0;
+        int qtdQuebrado = 1;
+
+        for(hora = 1; hora <= 16; hora++){
+
+            System.out.println(qtdQuebrado); 
+            qtdQuebrado = qtdQuebrado * 3;
+
+
+        }
+
+
+
+
+    }
+}
