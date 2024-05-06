63a6bd3743e8dc75874f0e77247a7fb590ca6bbf
SamuelCandido <samueljosecandido9@gmail.com>
2024-05-06 20:49:00 -0300

    14 Feito

--- Atividades/Unidade5/Uni5Exe-Java/atividades/Uni5Exe14.java
+++ Atividades/Unidade5/Uni5Exe-Java/atividades/Uni5Exe14.java
@@ -3,29 +3,53 @@ import java.util.Scanner;
 public class Uni5Exe14 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        double tCompra = 0, tVenda = 0;
+        int menor10 = 0, entre10E20 = 0, maior20 = 0;
+        double PC, PV, porcentLucro;
         
-
-        double PC = 0;
-        double PV = 0;
-        String nome = "";
-
 
         for (int i = 1; i <= 2; i++) {
 
-            System.out.println("Digite o nome do produto");
-            nome = scanner.nextLine();
-            
-            System.out.println("Digite o preço de compra da mercadoria " + i + ":");
+            System.out.println("Informe o nome de compra da mercadoria " + i + ": ");
+            String nome = scanner.next();
+
+            System.out.println("Informe o preço de compra da mercadoria " + i + ": ");
             PC = scanner.nextDouble();
 
-            System.out.println("Digite o preço de venda da mercadoria " + i + ":");
+            System.out.println("Informe o preço de venda da mercadoria " + i + ": ");
             PV = scanner.nextDouble();
 
+            porcentLucro = (PV - PC) / PC * 100;
 
+            System.out.println("Produto: "+ nome + " O preço de compra: "+ PC + " O preço de venda é: " + PV);
 
+            tCompra += PC;
+            tVenda += PV;
+
+            if (porcentLucro < 10) {
+             menor10++;
+            } 
+
+            else if (porcentLucro <= 20) {
+                entre10E20++;
+            } 
+
+            else {
+                maior20++;
+            }
         }
 
+        System.out.println();
+        System.out.println("Lucro < 10%: " + menor10);
+        System.out.println("Lucro entre 10% e 20%: " + entre10E20);
+        System.out.println("Lucro > 20%: " + maior20);
+
+        System.out.println("Total de compra: " + tCompra);
+        System.out.println("Total de venda: " + tVenda);
+        System.out.println("Lucro total: " +  (tVenda - tCompra));
 
         scanner.close();
     }
 }
+
