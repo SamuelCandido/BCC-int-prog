0aa5782db4e0441e08f463d32c50d772a66094cc
Samuel José Candido <samueljosecandido9@gmail.com>
2024-05-08 16:26:19 -0300

    atividade 19

---
+++ Atividades/Unidade5/Uni5Exe-Java/atividades/Uni5Exe19.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Uni5Exe19 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double desconto = 0.0;
+        double calculo = 0.0;
+        double total = 0.0;
+        
+        while (true) {
+        
+            System.out.println("Digite o valor da compra: ");
+            double vCompra = scanner.nextDouble();
+
+            if (vCompra == 0) {
+                System.out.println("Fechando caixa");
+                break;
+            }
+
+
+            if (vCompra <= 500) {
+                desconto = vCompra * 0.15;
+                calculo = vCompra - desconto;
+                total += calculo;
+                System.out.println("Apos o desconto de 15% você irá pagar: " + calculo);
+            }
+
+
+            else{
+                desconto = vCompra * 0.20;
+                calculo = vCompra - desconto;
+                total += calculo;
+                System.out.println("Apos o desconto de 20% você irá pagar: " + calculo);
+            }
+            
+        }
+
+        System.out.println("A loja ira receber no final do dia: "+ total +" reais");
+
+        scanner.close();
+    }
+}
