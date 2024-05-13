8377d2517c04b6fcad5f480462c79f6cac8b40ce
SamuelCandido <samueljosecandido9@gmail.com>
2024-05-13 20:56:17 -0300

    5 atividade 27

--- Atividades/Unidade5/Uni5Exe-Java/atividades/Uni5Exe27.java
+++ Atividades/Unidade5/Uni5Exe-Java/atividades/Uni5Exe27.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Uni5Exe27 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        double aux = 0.0;
         double soma = 0.0;
-        int totalP = 0;
-
-        System.out.print("Diga o valor por peça: ");
-        double valorP = scanner.nextDouble();
+        double total = 0;
+        int producao = 0;
+        int manha = 0;
+        int tarde = 0;
+        int maiorProducao = 0;
+        int guradaDia = 0;
 
         System.out.print("Novo funcionario (1 = sim | 2 = não)? ");
         int decisao = scanner.nextInt();
@@ -18,29 +21,57 @@ public class Uni5Exe27 {
             System.out.print("Digite o dia do mes de abril: ");
             int dia = scanner.nextInt();
 
+
             if (dia > 0 && dia < 31) {
                 
                 System.out.print("Digite a quantidade de peças de MANHA: ");
-                int manha = scanner.nextInt();
+                manha = scanner.nextInt();
 
                 System.out.print("Digite a quantidade de peças de TARDE: ");
-                int tarde = scanner.nextInt();
+                tarde = scanner.nextInt();
 
-                totalP = manha + tarde;
-                soma = totalP * valorP;
+                producao = manha + tarde;
 
-                System.out.println("O valor ganho no dia por produzir " + totalP + " peças foi: " + soma);
+                if (dia > 0 && dia <= 15) {
+                    if (manha > 30 && tarde > 30) {
+                        aux = 0.80;
+                        total = producao * aux;
+                    }
+                    else{
+                        aux = 0.50;
+                        total = producao * 0.50;
+                    }
+                }
 
+                else{
+                    total = manha * 0.40;
+                    total += tarde * 0.30;
+                }
+
+                System.out.println("O valor ganho no dia: " + total);
             }
 
             else{
                 System.out.println("Dia invalido");
             }
-            
-            System.out.print("Novo funcionario (1 = sim | 2 = não)? ");
-            decisao = scanner.nextInt();
+            if (maiorProducao <= producao) {
+                maiorProducao = producao;
+                guradaDia = dia;
+            }
 
+            
+            System.out.print("NOVO funcionario (1 = sim | 2 = não)? ");
+            decisao = scanner.nextInt();
         }
+        
+        System.out.print("O melhor dia foi o "+ guradaDia);
+                if (manha > tarde) {
+                    System.out.println("O periodo que mais produziu é o da manha: " + manha);
+                }
+                else{
+                    System.out.println(", e o periodo que mais produziu é o da tarde: " + tarde);
+                }
+
         scanner.close();
     }
 }
