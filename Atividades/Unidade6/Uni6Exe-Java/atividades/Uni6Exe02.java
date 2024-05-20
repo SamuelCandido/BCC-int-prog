a55db3098d0b446c9367685fce4fb19e6105a320
SamuelCandido <samueljosecandido9@gmail.com>
2024-05-20 20:03:17 -0300

    6 Atividade 2

--- Atividades/Unidade6/Uni6Exe-Java/atividades/Uni6Exe02.java
+++ Atividades/Unidade6/Uni6Exe-Java/atividades/Uni6Exe02.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+
 public class Uni6Exe02 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        double tenPosition[] = new double[3];
+        int cont = 1;
+        double soma = 0;
+        double media = 0.0;
+
+        for (int i = 2; i >= 0; i--){
+            System.out.print("Digite o valor do vetor "+ cont +": ");
+            tenPosition[i] = scanner.nextInt();
+            cont++;
+            soma += tenPosition[i];
+        
+        }
+        
+        media = soma / 3;
+        System.out.println("media "+ media);
+
+        for(double aux : tenPosition){
+            if (aux > media) {
+                System.out.println(aux);
+            }
+        }
+
+        scanner.close();
+    }
 }
