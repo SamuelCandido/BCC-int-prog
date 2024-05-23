6afcfca936392198f6804f67a1a7bc7ab3dcaf80
SamuelCandido <samueljosecandido9@gmail.com>
2024-05-22 21:32:16 -0300

    6 tem que revisar a resposta

--- Atividades/Unidade6/Uni6Exe-Java/atividades/Uni6Exe06.java
+++ Atividades/Unidade6/Uni6Exe-Java/atividades/Uni6Exe06.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+
 public class Uni6Exe06 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe o tamanho do vetor: ");
+        int tamanho = scanner.nextInt();
+
+        double[] vetor = new double[tamanho];
+        double esta = 0.0;
+
+        for (int i = 0; i < vetor.length; i++ ){
+            System.out.print("Informe o valor para povoar o vetor: ");
+            vetor[i] = scanner.nextDouble();
+        }
+
+        System.out.print("Digite um valor para ver se esta dentro do vetor: ");
+        esta = scanner.nextDouble();
+
+        for (int i = 0; i < vetor.length; i++ ){
+            if (vetor[i] == esta) {
+                System.out.println(true);
+            }
+            
+        }
+
+
+
+        scanner.close();
+    }
 }
