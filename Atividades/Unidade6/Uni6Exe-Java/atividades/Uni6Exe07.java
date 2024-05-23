14f63d9018e156fd3728a177848824b761643bb3
SamuelCandido <samueljosecandido9@gmail.com>
2024-05-23 20:57:50 -0300

    6 atividade 7

--- Atividades/Unidade6/Uni6Exe-Java/atividades/Uni6Exe07.java
+++ Atividades/Unidade6/Uni6Exe-Java/atividades/Uni6Exe07.java
@@ -1,3 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Uni6Exe07 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double[] vetor = new double[3];
+        double esta = 0.0;
+        int gambiarra = 1;
+
+        for (int i = 0; i < vetor.length; i++ ){
+            System.out.print("Informe o valor para povoar o vetor: ");
+            vetor[i] = scanner.nextDouble();
+        }
+
+        System.out.print("Digite um valor para ver se esta dentro do vetor: ");
+        esta = scanner.nextDouble();
+
+        for (int i = 0; i < vetor.length; i++ ){
+            if (vetor[i] == esta) {
+                gambiarra = 2;
+                System.out.println(true);
+            } 
+        }
+
+        if (gambiarra == 1) {
+            System.out.println(false);
+        } 
+
+        Arrays.sort(vetor);
+        System.out.println(Arrays.toString(vetor));
+
+        scanner.close();
+    }
 }
