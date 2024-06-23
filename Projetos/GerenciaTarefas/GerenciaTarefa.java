2c177ae37a9d24ce313e4646d79b06a4d425f0a3
SamuelCandido <samueljosecandido9@gmail.com>
2024-06-23 12:12:29 -0300

    feat: inicio

---
+++ Projetos/GerenciaTarefas/GerenciaTarefa.java
@@ -0,0 +1,20 @@
+package Projetos.GerenciaTarefas;
+
+public class GerenciaTarefa {
+    String titulo;
+    String corpo;
+    boolean concluida;
+
+    public void Tarefa(String titulo, String corpo){
+
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ Projetos/GerenciaTarefas/GerenciadordeTarefa.mk
@@ -0,0 +1,10 @@
+# Sistema de Gerenciamento de Tarefas
+
+## Descrição
+Aplicação para gerenciar tarefas.
+
+## Funcionalidades
+- Adicionar Tarefa: Permite adicionar uma nova tarefa com um título e uma descrição.
+- Listar Tarefas: Exibe todas as tarefas adicionadas.
+- Remover Tarefa: Permite remover uma tarefa pelo seu índice.
+- Marcar Tarefa como Concluída: Permite marcar uma tarefa como concluída.
